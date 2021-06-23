a144bb2e1baa6eee91bf2bd2ec5bc1cea691bdcf
z0972935669 <z0972935669@gmail.com>
2021-06-23 15:45:43 +0800

    add files

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <button @click="test">Test</button>
+    <button @click="test1">Test1</button>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    test() {
+      const api = `/test`;
+      this.$http.get(api).then((response) => {
+        console.log(response);
+      });
+    },
+    test1() {
+      const api1 = `/test1`;
+      this.$http.get(api1).then((response) => {
+        console.log(response);
+      });
+    }
+  },
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devServer: {
+      overlay: {
+        warnings: false, //不顯示警告
+        errors: false	// 不顯示錯誤
+      }
+    },
+    lintOnSave:false // 關閉eslint檢查
+}
